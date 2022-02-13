1184df2681e67b7c6823f5f93e3ca29742e7a003
lena <e_chugrova@mail.ru>
2022-02-13 22:05:27 +0300

    Initial commit

---
+++ src/com/company/Course.java
@@ -0,0 +1,27 @@
+package com.company;
+
+public class Course {
+
+    private int runDist;
+    private int jumpHeight;
+    private int crowlDist;
+
+    public Course(int runDist, int jumpHeight, int crowlDist) {
+        this.runDist = runDist;
+        this.jumpHeight = jumpHeight;
+        this.crowlDist = crowlDist;
+    }
+
+    int getRunDist() {
+        return this.runDist;
+    }
+
+    int getJumpHeight() {
+        return this.jumpHeight;
+    }
+
+    int getCrowlDist() {
+        return this.crowlDist;
+    }
+
+}
